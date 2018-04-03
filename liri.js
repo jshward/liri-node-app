7405d6825518c08b1a4c47767519bd69a89f7dd6
jshward <jshward069@gmail.com>
2018-04-03 01:57:45 -0700

    Spotify works, mostly

--- liri.js
+++ liri.js
@@ -59,14 +59,24 @@ switch (command) {
 	case "spotify-this-song":
 		//ok so this doesn't work
 		//var searchTerm = '"' + searchTerm + '"';
-		console.log(searchTerm);
-
-		spotify.search({ type: 'track', query: searchTerm, limit: 1 }, function (err, data) {
+		var searchTrack;
+		if (searchTerm === undefined) {
+			searchTrack = "What's My Age Again?";
+		} else {
+			searchTrack = searchTerm;
+		}
+		//launch spotify search
+		spotify.search({ type: 'track', query: searchTrack }, function (err, data) {
 			if (err) {
-				return console.log('Error occurred: ' + err);
+				console.log('Error occurred: ' + err);
+				return;
+			} else {
+				//tried searching for release year! Spotify doesn't return this!
+				console.log("Artist: " + data.tracks.items[0].artists[0].name);
+				console.log("Song: " + data.tracks.items[0].name);
+				console.log("Album: " + data.tracks.items[0].album.name);
+				console.log("Preview Here: " + data.tracks.items[0].preview_url);
 			}
-
-			console.log(JSON.parse(data).tracks.items.album.artists.name);
 		});
 	//case "do-what-it-says"
 	default:
